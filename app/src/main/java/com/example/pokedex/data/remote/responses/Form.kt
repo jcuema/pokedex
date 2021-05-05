51779dce24f23e471b6c4afebb2d5816762e2822
Jean Uema <jcuema@gmail.com>
2021-05-05 09:43:57 -0300

    add remotes

---
+++ app/src/main/java/com/example/pokedex/data/remote/responses/Form.kt
@@ -0,0 +1,7 @@
+package com.example.pokedex.data.remote.responses
+
+
+data class Form(
+    val name: String,
+    val url: String
+)
--- app/src/main/java/com/example/pokedex/data/remote/responses/PokemonList.kt
+++ app/src/main/java/com/example/pokedex/data/remote/responses/PokemonList.kt
@@ -1,4 +1,8 @@
 package com.example.pokedex.data.remote.responses
 
-class PokemonList {
-}
+data class PokemonList(
+    val count: Int,
+    val next: String,
+    val previous: Any,
+    val results: List<Result>
+)
